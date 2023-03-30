ee98bc97e1046437758c5c46905972b9a620feb3
Kang <rkdalsgur032@unist.ac.kr>
2023-03-30 19:28:19 +0900

    builder

---
+++ Creational Patterns/Builder/index.ts
@@ -0,0 +1,93 @@
+interface Builder {
+  producePartA(): void;
+  producePartB(): void;
+  producePartC(): void;
+}
+
+class ConcreteBuilder1 implements Builder {
+  private product!: Product1;
+
+  constructor() {
+    this.reset();
+  }
+
+  public reset(): void {
+    this.product = new Product1();
+  }
+
+  public producePartA(): void {
+    this.product.parts.push("PartA1");
+  }
+
+  public producePartB(): void {
+    this.product.parts.push("PartB1");
+  }
+
+  public producePartC(): void {
+    this.product.parts.push("PartC1");
+  }
+
+  public getProduct(): Product1 {
+    const result = this.product;
+    this.reset();
+    return result;
+  }
+}
+
+class Product1 {
+  public parts: string[] = [];
+
+  public listParts(): void {
+    console.log(`Product parts: ${this.parts.join(", ")}\n`);
+  }
+}
+
+class Director {
+  private builder!: Builder;
+
+  public setBuilder(builder: Builder): void {
+    this.builder = builder;
+  }
+
+  public buildMinimalViableProduct(): void {
+    this.builder.producePartA();
+  }
+
+  public buildFullFeaturedProduct(): void {
+    this.builder.producePartA();
+    this.builder.producePartB();
+    this.builder.producePartC();
+  }
+}
+
+function clientCode(director: Director) {
+  const builder = new ConcreteBuilder1();
+  director.setBuilder(builder);
+
+  console.log("Standard basic product:");
+  director.buildMinimalViableProduct();
+  builder.getProduct().listParts();
+
+  console.log("Standard full featured product:");
+  director.buildFullFeaturedProduct();
+  builder.getProduct().listParts();
+
+  console.log("Custom product:");
+  builder.producePartA();
+  builder.producePartC();
+  builder.getProduct().listParts();
+}
+
+const director = new Director();
+clientCode(director);
+
+/** 실행 결과
+ * Standard basic product:
+ * Product parts: PartA1
+ *
+ * Standard full featured product:
+ * Product parts: PartA1, PartB1, PartC1
+ *
+ * Custom product:
+ * Product parts: PartA1, PartC1
+ */
